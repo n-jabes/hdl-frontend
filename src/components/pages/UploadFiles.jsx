d5e92b62a712c88926b026483ba231185e990789
n-jabes <nshutij7@gmail.com>
2024-10-08 15:40:02 +0200

    removed readxlsx file

--- src/components/pages/UploadFiles.jsx
+++ src/components/pages/UploadFiles.jsx
@@ -1,20 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { parseExcelData } from '../../utils/readxlsx';
 
 function UploadFiles() {
   const [file, setFile] = useState(null);
-  const [coreAreasFile, setCoreAreasFile] = useState(null); // Add state to store the file
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
   };
 
-  // Handle file selection
-  const handleCoreAreasChange = (e) => {
-    const selectedFile = e.target.files[0]; // Get the first file from input
-    setCoreAreasFile(selectedFile); // Set the selected file in state
-  };
+  
 
   const UploadSubscribers = async (e) => {
     e.preventDefault(); // Prevent form submission from reloading the page
@@ -54,31 +48,6 @@ function UploadFiles() {
     }
   };
 
-  // Function to handle the file upload
-  const UploadCoreAreas = async (e) => {
-    e.preventDefault(); // Prevent form submission from reloading the page
-
-    if (!coreAreasFile) {
-      alert('Please select a file to upload.');
-      return;
-    }
-
-    const reader = new FileReader();
-
-    // This function is called once the file is read
-    reader.onload = async (event) => {
-      const fileBuffer = event.target.result;
-
-      // Parse the file buffer
-      const coreAreas = await parseExcelData(fileBuffer);
-      console.log('Core Areas: ', coreAreas);
-
-      // You can now send the coreAreas data to the backend or handle it further
-    };
-
-    // Read the file as ArrayBuffer
-    reader.readAsArrayBuffer(coreAreasFile);
-  };
 
   return (
     <div className="bg-[#1A1D1F] p-3">
@@ -100,24 +69,7 @@ function UploadFiles() {
           Upload
         </button>
       </form>
-      {/* upload core areas */}
-      <form onSubmit={UploadCoreAreas} className="text-sm mt-8">
-        <h2 className="mb-2">Upload Core Areas</h2>
-        <div className="my-2">
-          <input
-            type="file"
-            accept=".xls, .xlsx"
-            onChange={handleCoreAreasChange}
-            className="w-max cursor-pointer"
-          />
-        </div>
-        <button
-          type="submit"
-          className="mt-2 w-max px-4 py-2 bg-mainBlue hover:bg-blue-500 text-white rounded"
-        >
-          Upload
-        </button>
-      </form>
+      
     </div>
   );
 }
